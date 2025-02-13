385cc42eb95693e967bc041da002f14f511142ea
Mathis Borges <mathis.bc23@gmail.com>
2025-02-13 15:59:15 +0100

    Affichage musics

--- app/src/Controller/MusicController.php
+++ app/src/Controller/MusicController.php
@@ -5,14 +5,18 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\MusicRepository;
 
 final class MusicController extends AbstractController
 {
     #[Route('/', name: 'app_music')]
-    public function index(): Response
+    public function index(
+        MusicRepository $musicRepo 
+    ): Response
     {
+        $musics = $musicRepo->findAll();
         return $this->render('music/index.html.twig', [
-            'controller_name' => 'MusicController',
+            'musicsList' => $musics,
         ]);
     }
 }
